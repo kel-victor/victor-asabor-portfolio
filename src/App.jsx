4f61a9a98994013e76319349609eae1468546f9c
vic552 <asaborvictor86@gmail.com>
2025-07-10 15:28:35 +0100

    🔐 Add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import  ThemeProvider  from './components/ThemeProvider.jsx'
+import ThemeProvider from './components/ThemeProvider.jsx'
 import Navbar from './components/Navbar.jsx'
 import Home from './pages/Home.jsx'
 import Blog from './pages/Blog.jsx'
 import NotFound from './pages/NotFound.jsx'
 import FloatingWhatsApp from './components/FloatingWhatsApp.jsx'
-
+import CookieConsent from "react-cookie-consent";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<Blog />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-          <FloatingWhatsApp />   {/* <- always visible */}
-      </ThemeProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <ThemeProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:slug" element={<Blog />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <FloatingWhatsApp />
+        </ThemeProvider>
+      </BrowserRouter>
+
+      {/* ✅ Cookie Consent at bottom */}
+      <CookieConsent
+        location="bottom"
+        buttonText="I understand"
+        cookieName="victorAsaborCookieConsent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#fff", background: "#e91e63", fontSize: "13px" }}
+        expires={150}
+      >
+        This website uses cookies to enhance user experience and for analytics.
+      </CookieConsent>
+    </>
   )
 }
